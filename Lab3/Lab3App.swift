604630d88b81976a9a6d2004a55a608436196227
Joshua Tan <jxt208@nyu.edu>
2025-11-08 17:07:00 +0800

    Initial Commit

---
+++ Lab3/Lab3App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab3App.swift
+//  Lab3
+//
+//  Created by Joshua Tan on 2025/11/8.
+//
+
+import SwiftUI
+
+@main
+struct Lab3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lab3Tests/Lab3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Lab3Tests.swift
+//  Lab3Tests
+//
+//  Created by Joshua Tan on 2025/11/8.
+//
+
+import Testing
+@testable import Lab3
+
+struct Lab3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
